48cf28eecd21b404588ae77a104d26e5686e395c
luiszaba <luis.zabala076@gmail.com>
2019-02-06 15:43:27 -0500

    need to scrape data

--- lib/Tech_News/intel_news.rb
+++ lib/Tech_News/intel_news.rb
@@ -1,13 +1,74 @@
-class TechNews::Deal
-    attr_accessor :title, :date, :source
+class TechNews::News
+    attr_accessor :title, :icon, :url
 
 def self.today
-    #I should return a bunch of instances of news
-    puts <<-DOC.gsub /^\s*/, ''
-    1. Intel did this
-    2. AMD did this
-    3. NVidia did this
-    DOC
+    self.scrape_news
+end
 
+self.scrape_news
+news = []
+
+news << self.scrape_intel
+news << self.scrape_amd
+news << self.scrape_apple
+news << self.scrape_wcctech
+news << self.scrape_microsoft
+
+news
+end
+
+def self.scrape_intel
+    doc = Nokogiri::HTML(open("#url"))
+
+    news = self.new
+    news.icon = doc.search().strip
+    news.title = doc.search().text.strip
+    news.url - doc.search().strip
+
+    news
+end
+
+def self.scrape_amd
+    doc = Nokogiri::HTML(open("#url"))
+
+    news = self.new
+    news.icon = doc.search().strip
+    news.title = doc.search().text.strip
+    news.url - doc.search().strip
+
+    news
+end
+
+def self.scrape_apple
+    doc = Nokogiri::HTML(open("#url"))
+
+    news = self.new
+    news.icon = doc.search().strip
+    news.title = doc.search().text.strip
+    news.url - doc.search().strip
+
+    news
+end
+
+def self.scrape_wcctech
+    doc = Nokogiri::HTML(open("#url"))
+
+    news = self.new
+    news.icon = doc.search().strip
+    news.title = doc.search().text.strip
+    news.url - doc.search().strip
+
+    news
+end
+
+def self.scrape_microsoft
+    doc = Nokogiri::HTML(open("#url"))
+
+    news = self.new
+    news.icon = doc.search().strip
+    news.title = doc.search().text.strip
+    news.url - doc.search().strip
+
+    news
 end
 end
