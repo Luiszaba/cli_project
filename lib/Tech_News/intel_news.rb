53524a6a128a6f49c0fa479d4bc43e3cf47aa5a6
luiszaba <luis.zabala076@gmail.com>
2019-02-06 22:27:48 -0500

    scrap tests in progress. Intel site hard to work with.  Will finish project tomorrow.

--- lib/Tech_News/intel_news.rb
+++ lib/Tech_News/intel_news.rb
@@ -6,19 +6,23 @@ class TechNews::News
 
     attr_accessor :title, :icon, :url
 
+    @@all = []
+
 def self.today
     scrape_news
 end
 
+def self.save
+    @@all << self
+end
+
 def self.scrape_news
 news = []
-
 news << self.scrape_intel
 news << self.scrape_amd
 news << self.scrape_apple
 news << self.scrape_wcctech
 news << self.scrape_microsoft
-
 news
 end
 
@@ -29,51 +33,47 @@ binding.pry
     news.icon = doc.search().strip
     news.title = doc.search().text.strip
     news.url - doc.search().strip
-
     news
 end
 
+#Title = doc.search("div.post-title")
+#Date = doc.search("div.post-timestamp")
+#url = doc.search("a.post-link")
+
+
 def self.scrape_amd
     doc = Nokogiri::HTML(open("https://www.amd.com/en/corporate/newsroom"))
-
     news = self.new
     news.icon = doc.search().strip
     news.title = doc.search().text.strip
     news.url - doc.search().strip
-
     news
 end
 
 def self.scrape_apple
     doc = Nokogiri::HTML(open("https://www.apple.com/newsroom/"))
-
     news = self.new
     news.icon = doc.search().strip
     news.title = doc.search().text.strip
     news.url - doc.search().strip
-
     news
 end
 
 def self.scrape_wcctech
     doc = Nokogiri::HTML(open("https://wccftech.com/"))
-
     news = self.new
     news.icon = doc.search().strip
     news.title = doc.search().text.strip
     news.url - doc.search().strip
-
     news
 end
 
 def self.scrape_microsoft
     doc = Nokogiri::HTML(open("https://news.microsoft.com/category/press-releases/"))
-
     news = self.new
     news.icon = doc.search().strip
     news.title = doc.search().text.strip
     news.url - doc.search().strip
-
     news
 end
 end
