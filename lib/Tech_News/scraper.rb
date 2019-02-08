d4556074c14e652967505599f790420b1c744444
luiszaba <luis.zabala076@gmail.com>
2019-02-08 13:29:27 -0500

    intel_news_almost_finished

--- lib/Tech_News/scraper.rb
+++ lib/Tech_News/scraper.rb
@@ -3,39 +3,22 @@ require 'open-uri'
 require 'pry'
 
 
-class Newsroom
-    attr_accessor :icon, :date, :title, :url
 
-    @@all = []
-
-def initialize
-    arrange_scrape
-    @@all << self
-end
-
-def self.all
-    @@all
-end
-
-def self.save
-    @@all << self
-end
-
-def arrange_scrape
     html = "https://newsroom.intel.com/"
     scraper = {}
     doc = Nokogiri::HTML(open(html))
     doc.css(".container").each do |news|
-        binding.pry
+
+    
+
     newsroom = news.css(".container").text
+    
     scraper[newsroom.to_sym] = {
     :icon => doc.css("div a img").attribute('src').value,
     :title => doc.css("div.post-title").first.text.strip,
     :url => doc.css("div.post-title").first.text.strip,
     :date => doc.css(".post-timestamp").first.text.strip,
     }
-end
     newsroom
-
- end
+    binding.pry
 end
